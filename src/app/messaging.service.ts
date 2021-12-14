d1ddc80dac8a5c5b778c3244f95d96b195cd2388
Nakesto <vinboyhuang88@gmail.com>
2021-12-14 14:32:40 +0700

    update

--- src/app/messaging.service.ts
+++ src/app/messaging.service.ts
@@ -14,26 +14,26 @@ export class MessagingService {
   constructor(private http: HttpClient) { }
 
   getMessaging(message: string, id: string) {
-    let url = "https://api.telegram.org/bot5035666714:AAFiboQ-1LBElXBJQ8TBl8kPTjQWluF--uk/sendMessage?chat_id=" + id + "&text=" + message;
+    let url = "https://api.telegram.org/bot5025951968:AAHWHjD1BGTMQs6qH8RsjCcRwtSSFkeLVAo/sendMessage?chat_id=" + id + "&text=" + message;
     return this.http.get(url);
   }
 
   getUpdates() {
-    let url = 'https://api.telegram.org/bot5035666714:AAFiboQ-1LBElXBJQ8TBl8kPTjQWluF--uk/getUpdates';
+    let url = 'https://api.telegram.org/bot5025951968:AAHWHjD1BGTMQs6qH8RsjCcRwtSSFkeLVAo/getUpdates';
     return this.http.get(url);
   }
 
   getMe() {
-    let url = 'https://api.telegram.org/bot5035666714:AAFiboQ-1LBElXBJQ8TBl8kPTjQWluF--uk/getMe';
+    let url = 'https://api.telegram.org/bot5025951968:AAHWHjD1BGTMQs6qH8RsjCcRwtSSFkeLVAo/getMe';
     return this.http.get(url);
   }
 
   location(id: string) {
-    let url = `https://api.telegram.org/bot5035666714:AAFiboQ-1LBElXBJQ8TBl8kPTjQWluF--uk/sendLocation?latitude=-6.2552333&longitude=106.6164091&chat_id=${id}`
+    let url = `https://api.telegram.org/bot5025951968:AAHWHjD1BGTMQs6qH8RsjCcRwtSSFkeLVAo/sendLocation?latitude=-6.2552333&longitude=106.6164091&chat_id=${id}`
     return this.http.get(url);
   }
   parseMode(id: string, message: string) {
-    let url = `https://api.telegram.org/bot5035666714:AAFiboQ-1LBElXBJQ8TBl8kPTjQWluF--uk/sendMessage?chat_id=${id}&text=${message}&parse_mode=HTML`
+    let url = `https://api.telegram.org/bot5025951968:AAHWHjD1BGTMQs6qH8RsjCcRwtSSFkeLVAo/sendMessage?chat_id=${id}&text=${message}&parse_mode=HTML`
     return this.http.get(url);
   }
 
